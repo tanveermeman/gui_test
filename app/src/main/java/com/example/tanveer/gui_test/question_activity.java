914f1f951b0be22c3b5d8cbff6d8175f2a2037ac
Karim <karim@browserstack.com>
2017-11-19 21:46:33 +0530

    adding firebase push

--- app/src/main/java/com/example/tanveer/gui_test/question_activity.java
+++ app/src/main/java/com/example/tanveer/gui_test/question_activity.java
@@ -7,17 +7,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Random;
 
 
 public class question_activity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
 
+    private int getRandomNumber(int min,int max) {
+        return (new Random()).nextInt((max - min) + 1) + min;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_activity);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         final Button button = findViewById(R.id.SaveQButton);
         final EditText question = findViewById(R.id.QuestionBox);
         final EditText choiceA = findViewById(R.id.ChoiceABox);
@@ -26,25 +34,25 @@ public class question_activity extends AppCompatActivity {
         final EditText choiceD = findViewById(R.id.ChoiceDBox);
         final EditText correctAnswer = findViewById(R.id.CorrectAnswer);
 
+        final HashMap<String, String > myquestion;
+        myquestion = new HashMap<String, String>();
 
-
-        // Initialize Array of lenght 6.
-        // array[0] = myquestion
-        // array[1-4] = myquestion
-        // array[5] = correct answer
-
-        final String[] myquestion = new String[6];
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                myquestion[0] = question.getText().toString();
-                myquestion[1] = choiceA.getText().toString();
-                myquestion[2] = choiceB.getText().toString();
-                myquestion[3] = choiceC.getText().toString();
-                myquestion[4] = choiceD.getText().toString();
-                myquestion[5] = correctAnswer.getText().toString();
-                 Log.d("ADebugTag", "myquestion : " + Arrays.toString(myquestion));
+                final int randomInt;
+                randomInt = getRandomNumber(1, 10000);
+                myquestion.put("question", question.getText().toString());
+                myquestion.put("choiceA", choiceA.getText().toString());
+                myquestion.put("choiceB", choiceB.getText().toString());
+                myquestion.put("choiceC", choiceC.getText().toString());
+                myquestion.put("choiceD", choiceD.getText().toString());
+
+                myquestion.put("correctAnswer", correctAnswer.getText().toString());
+                // Log.d("ADebugTag", "myquestion : " + Arrays.toString(myquestion));
+                 mDatabase.child("questions").child(Integer.toString(randomInt)).setValue(myquestion);
             }
+
 
         });
 
